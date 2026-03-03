8ff543c7c3032dccdf480421efc08b54ca6034a8
Uk2476 <kuttkarsh028@gmail.com>
2026-03-03 19:42:35 +0530

    fixed errors

--- script/InteractionBasicNft.s.sol
+++ script/InteractionBasicNft.s.sol
@@ -12,10 +12,10 @@ contract MintBasicNft is Script{
 
     function run () external {
         address DeployedAddress = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
-        mintBftOncontract(DeployedAddress);
+        mintNftOnContract(DeployedAddress);
     }
 
-    function mnitNftOnContract(address memory basicNftAddress) public {
+    function mintNftOnContract(address memory basicNftAddress) public {
         vm.startBroadcast();
         BasicNft(basicNftAddress).mintNft(PUG_URI);
         vm.stopBroadcast();

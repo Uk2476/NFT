2dac2071f4ecaf23da45854f365d1771f8041a4a
Uk2476 <kuttkarsh028@gmail.com>
2026-03-03 19:55:03 +0530

    contract for an svg

---
+++ src/MoodNft.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT 
+pragma solidity ^0.8.18;
+
+import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "../lib/openzeppelin-contracts/contracts/utils/Base64.sol";
+
+contract MoodNft is ERC721 {
+    
+}

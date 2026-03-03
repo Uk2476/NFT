84a5a89b62841b9720da602805a13149252a3a14
Uk2476 <kuttkarsh028@gmail.com>
2026-03-03 18:57:01 +0530

    constructor

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -4,6 +4,13 @@ pragma solidity ^0.8.18;
 import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721{
-    contr
+    uint256 private s_tokenCounter;
+    mapping (uint256 => string ) private s_tokenIdToUri ;
+
+    constructor() ERC721 ("DOGIE" , "DOG"){
+        S_tokenCounter = 0;
+    }
+
+    
 }
 

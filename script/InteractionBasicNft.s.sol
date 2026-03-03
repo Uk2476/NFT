dad90c1f76560737ba3c9b3a2d676899f36515d5
Uk2476 <kuttkarsh028@gmail.com>
2026-03-03 19:41:18 +0530

    addes exxtra things

--- script/InteractionBasicNft.s.sol
+++ script/InteractionBasicNft.s.sol
@@ -7,14 +7,17 @@ import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 
 contract MintBasicNft is Script{
 
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     function run () external {
         address DeployedAddress = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
+        mintBftOncontract(DeployedAddress);
     }
 
     function mnitNftOnContract(address memory basicNftAddress) public {
         vm.startBroadcast();
-        BasicNft(basicNftAddress).mintNft();
+        BasicNft(basicNftAddress).mintNft(PUG_URI);
         vm.stopBroadcast();
     }
 }
